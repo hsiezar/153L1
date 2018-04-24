9f2a64eb233de9420c6ebeb709f0cd6fb1315f69
Andrew To <ato004@sledge.cs.ucr.edu>
2018-04-23 23:01:15 -0700

    Added lab 1 comments

--- proc.h
+++ proc.h
@@ -49,7 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16]
-  int exitStatus;               // Process name (debugging)
+  int exitStatus;               // LAB 1 Process name (debugging)
 };
 
 // Process memory is laid out contiguously, low addresses first:
