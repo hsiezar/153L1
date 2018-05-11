694002f40f7054a89a4104131024d3a32afa08eb
Andrew To <ato004@sledge.cs.ucr.edu>
2018-05-10 18:07:09 -0700

    fixed indentation

--- proc.c
+++ proc.c
@@ -375,6 +375,7 @@ void
 scheduler(void)
 {
   struct proc *p;
+  struct proc *q;
   struct cpu *c = mycpu();
   c->proc = 0;
   struct proc *smallPrior;
@@ -390,13 +391,13 @@ scheduler(void)
         continue;
 	smallPrior = p;
 
-    for(q = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(q->state != RUNNABLE)
-	continue;
-      if(smallPrior->priority > q->priority){
-	smallPrior = q;
+      for(q = ptable.proc; p < &ptable.proc[NPROC]; p++){
+        if(q->state != RUNNABLE)
+	  continue;
+        if(smallPrior->priority > q->priority){
+	  smallPrior = q;
+        }
       }
-    }
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
