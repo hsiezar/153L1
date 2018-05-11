4d02cd1fdff1e70cc2e29d82c77c8fb8f1961b14
Andrew To <ato004@sledge.cs.ucr.edu>
2018-05-10 18:02:08 -0700

    minor changes

--- defs.h
+++ defs.h
@@ -106,6 +106,8 @@ int             pipewrite(struct pipe*, char*, int);
 int             cpuid(void);
 void            exit(int); //LAB1 CS153
 int             fork(void);
+int		getPriority(int);
+int		setPriority(int);
 int             growproc(int);
 int             kill(int);
 struct cpu*     mycpu(void);
--- lab2.c
+++ lab2.c
@@ -1,5 +1,6 @@
 #include "types.h"
 #include "user.h"
+#include "stat.h"
 
 int main(int argc, char *argv[])
 {
