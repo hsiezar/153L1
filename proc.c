d49177e442dbe7a74a613186ed9af5dd99eb4b56
Andrew To <ato004@sledge.cs.ucr.edu>
2018-05-10 18:23:32 -0700

    made fixes to scheduler

--- proc.c
+++ proc.c
@@ -386,34 +386,36 @@ scheduler(void)
 
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
-	smallPrior = p;
-
-      for(q = ptable.proc; p < &ptable.proc[NPROC]; p++){
-        if(q->state != RUNNABLE)
+	smallPrior = 0;
+        int min_priority = 10000;
+      for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+        if(p->state != RUNNABLE)
 	  continue;
-        if(smallPrior->priority > q->priority){
-	  smallPrior = q;
+        //q->priority++;
+        if(min_priority > p->priority){
+	  smallPrior = p;
+          min_priory = p->priority;
         }
+        p->priority++;
       }
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
+      if(smallPrior != 0) {
+      c->proc = smallPrior;
+      switchuvm(smallPrior);
+      smallPrior->state = RUNNING;
+      smallPrior->priority -= 2;
+      swtch(&(c->scheduler), smallPrior->context);
       switchkvm();
+    
 
       // Process is done running for now.
       // It should have changed its p->state before coming back.
       c->proc = 0;
+      }
     }
     release(&ptable.lock);
-
   }
 }
 
